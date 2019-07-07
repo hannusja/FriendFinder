a99dd62552d7290395e26ec4f1d5e1288ff5143c
unknown <hannahuseva@gmail.com>
2019-07-07 15:14:30 -0500

    Survey done. Validation done. Needs match logic.

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+var friends = 
+[{ 
+    name:"Cute doggy",
+    photo:"https://drive.google.com/file/d/1mRRSrvU08Qvm1FYXb8S0pFYGsvf7dGAl/view?usp=sharing",
+    scores:[5,1,4,4,5,1,2,5,4,1]
+}]
+
+module.exports = friends
