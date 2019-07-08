0c625ee5f6b683f97cfa3d1057960fa1891c3a75
unknown <hannahuseva@gmail.com>
2019-07-07 19:12:43 -0500

    Polishing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 var friends = 
 [{ 
     name:"Cute doggy",
-    photo:"https://raw.githubusercontent.com/yanjasho/yanjasho.github.io/master/assets/images/background.jpg",
+    photo:"https://raw.githubusercontent.com/yanjasho/FriendFinder/master/app/public/cute_doggy.jpg",
     scores:[5,1,1,1,1,1,5,1,3,5]
 },
 { 
     name:"Adorable doggy",
-    photo:"https://raw.githubusercontent.com/yanjasho/yanjasho.github.io/master/assets/images/background.jpg",
+    photo:"https://raw.githubusercontent.com/yanjasho/FriendFinder/master/app/public/adorable_doggy.jpg",
     scores:[5,1,1,1,1,1,1,5,3,1]
 }]
 
